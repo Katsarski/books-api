a570d362aec37d6f861a9a38e70de4edd4e946e6
Katsarski <boyankatsarski@gmail.com>
2025-06-11 20:08:20 +0300

    added logger, replaced allure with pytest-html for reporting

--- utils/request_handler.py
+++ utils/request_handler.py
@@ -1,23 +1,28 @@
 import requests
+from utils.logger import log_request_response
 
 class APIClient:
     def __init__(self, base_url):
         self.base_url = base_url
         self.session = requests.Session()
-        self.session.headers.clear()  # Remove all default headers
+        self.session.headers.clear()
 
+    @log_request_response("GET")
     def get(self, endpoint, **kwargs):
-        headers = kwargs.pop("headers", {})  # allow for sendingcustom headers
+        headers = kwargs.pop("headers", {})
         return self.session.get(f"{self.base_url}{endpoint}", headers=headers, **kwargs)
 
+    @log_request_response("POST")
     def post(self, endpoint, data=None, **kwargs):
         headers = kwargs.pop("headers", {})
         return self.session.post(f"{self.base_url}{endpoint}", headers=headers, json=data, **kwargs)
 
+    @log_request_response("PUT")
     def put(self, endpoint, data=None, **kwargs):
         headers = kwargs.pop("headers", {})
         return self.session.put(f"{self.base_url}{endpoint}", headers=headers, json=data, **kwargs)
 
+    @log_request_response("DELETE")
     def delete(self, endpoint, **kwargs):
         headers = kwargs.pop("headers", {})
         return self.session.delete(f"{self.base_url}{endpoint}", headers=headers, **kwargs)
